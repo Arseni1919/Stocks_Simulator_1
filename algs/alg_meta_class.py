ee003d7ebf4d3e6a85379b58d55f2a0edbf255c0
perchik_air <1919ars@gmail.com>
2023-02-21 22:32:26 +0200

    more

--- algs/alg_meta_class.py
+++ algs/alg_meta_class.py
@@ -2,7 +2,8 @@ from globals import *
 
 
 class MetaAlg(ABC):
-
+    # TODO: to transfer the main part of algs to this class
+    # TODO: to make available multiple action calls
     def __init__(self):
         self.name = 'Alg'
 

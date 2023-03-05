b3919e0bac84e64e4a896d6f26785902068cb8f1
perchik_air <1919ars@gmail.com>
2023-03-05 20:42:55 +0200

    more

--- algs/alg_buy_low_sell_high.py
+++ algs/alg_buy_low_sell_high.py
@@ -7,36 +7,11 @@ from plot_fucntions_and_classes.plot_functions import *
 class BuyLowSellHighAlg(MetaAlg):
 
     def __init__(self, env, to_plot=False, params=None):
-        super().__init__()
+        super().__init__(env, to_plot, params)
         # init
         if params is None:
-            params = {'w1': 40, 'w2': 20}
-        self.env = env
-        self.params = params
+            self.params = {'w1': 40, 'w2': 20}
         self.name = f'BLSH-{self.params["w1"]}-{self.params["w2"]}'
-        self.to_plot = to_plot
-        self.max_steps = self.env.max_steps
-        # global data
-        self.list_of_assets = self.env.list_of_assets
-        self.main_asset = self.list_of_assets[0]
-        self.history_assets = {asset: np.zeros((self.max_steps,)) for asset in self.list_of_assets}
-        self.history_volume = {asset: np.zeros((self.max_steps,)) for asset in self.list_of_assets}
-        # agents data
-        self.history_actions = np.zeros((self.max_steps,))
-        self.history_cash = np.zeros((self.max_steps,))
-        self.history_holdings = np.zeros((self.max_steps,))
-        self.history_holdings_worth = np.zeros((self.max_steps,))
-        self.history_orders = np.zeros((self.max_steps,))
-        self.history_portfolio_worth = np.zeros((self.max_steps,))
-        self.history_commission_value = np.zeros((self.max_steps,))
-        self.history_property = np.zeros((self.max_steps,))
-        self.history_termination = np.zeros((self.max_steps,))
-        # for plots
-        if self.to_plot:
-            self.subplot_rows = 2
-            self.subplot_cols = 3
-            self.fig, self.ax = plt.subplots(self.subplot_rows, self.subplot_cols, figsize=(14, 7))
-            self.ax_volume = self.ax[0, 0].twinx()
 
     @staticmethod
     def action_decision(in_hand, det_big, det_small):
@@ -76,35 +51,11 @@ class BuyLowSellHighAlg(MetaAlg):
 
         return [(self.main_asset, action[0]), (self.main_asset, action[1])]
 
-    def update_history(self, observation):
-        # current state data:
-        step_count = observation['step_count']
-        in_hand = observation['in_hand'][self.main_asset]
-        # global data:
-        for asset in self.list_of_assets:
-            self.history_assets[asset][step_count] = observation['asset'][asset]
-            self.history_volume[asset][step_count] = observation['asset_volume'][asset]
-        # agent data:
-        self.history_cash[step_count] = observation['history_cash']
-        self.history_holdings[step_count] = observation['history_holdings']
-        self.history_holdings_worth[step_count] = observation['history_holdings_worth']
-        self.history_orders[step_count] = observation['history_orders']
-        self.history_portfolio_worth[step_count] = observation['history_portfolio_worth']
-        self.history_commission_value[step_count] = observation['history_commission_value']
-        return step_count, in_hand
-
     def update_after_action(self, observation, action, portfolio_worth, next_observation, terminated, truncated):
         step_count = observation['step_count']
         last_action_asset, last_action_value = action[-1]
         self.history_actions[step_count] = last_action_value
         self.history_termination[step_count] = terminated
-
-    def render(self, info):
-        if self.to_plot:
-            info['main_asset'] = self.main_asset
-            self.env.render_graphs(self.ax, self.ax_volume, info)
-            self.fig.suptitle(f'Sim: {self.env.name}, Alg: {self.name}', fontsize=16)
-            plt.pause(0.001)
 
 
 def main():
--- algs/alg_meta_class.py
+++ algs/alg_meta_class.py
@@ -2,10 +2,33 @@ from globals import *
 
 
 class MetaAlg(ABC):
-    # TODO: to transfer the main part of algs to this class
     # TODO: to make available multiple action calls
-    def __init__(self):
+    def __init__(self, env, to_plot=False, params=None):
         self.name = 'Alg'
+        self.env = env
+        self.params = params
+        self.to_plot = to_plot
+        self.max_steps = self.env.max_steps
+        # global data
+        self.list_of_assets = self.env.list_of_assets
+        self.main_asset = self.list_of_assets[0]
+        self.history_assets = {asset: np.zeros((self.max_steps,)) for asset in self.list_of_assets}
+        self.history_volume = {asset: np.zeros((self.max_steps,)) for asset in self.list_of_assets}
+        # agents data
+        self.history_actions = np.zeros((self.max_steps,))
+        self.history_cash = np.zeros((self.max_steps,))
+        self.history_holdings = np.zeros((self.max_steps,))
+        self.history_holdings_worth = np.zeros((self.max_steps,))
+        self.history_orders = np.zeros((self.max_steps,))
+        self.history_portfolio_worth = np.zeros((self.max_steps,))
+        self.history_commission_value = np.zeros((self.max_steps,))
+        self.history_termination = np.zeros((self.max_steps,))
+        # for plots
+        if self.to_plot:
+            self.subplot_rows = 2
+            self.subplot_cols = 3
+            self.fig, self.ax = plt.subplots(self.subplot_rows, self.subplot_cols, figsize=(14, 7))
+            self.ax_volume = self.ax[0, 0].twinx()
 
     @abstractmethod
     def return_action(self, observation):
@@ -19,6 +42,26 @@ class MetaAlg(ABC):
     def update_after_action(self, observation, action, reward, next_observation, terminated, truncated):
         pass
 
-    @abstractmethod
+    def update_history(self, observation):
+        # current state data:
+        step_count = observation['step_count']
+        in_hand = observation['in_hand'][self.main_asset]
+        # global data:
+        for asset in self.list_of_assets:
+            self.history_assets[asset][step_count] = observation['asset'][asset]
+            self.history_volume[asset][step_count] = observation['asset_volume'][asset]
+        # agent data:
+        self.history_cash[step_count] = observation['history_cash']
+        self.history_holdings[step_count] = observation['history_holdings']
+        self.history_holdings_worth[step_count] = observation['history_holdings_worth']
+        self.history_orders[step_count] = observation['history_orders']
+        self.history_portfolio_worth[step_count] = observation['history_portfolio_worth']
+        self.history_commission_value[step_count] = observation['history_commission_value']
+        return step_count, in_hand
+
     def render(self, info):
-        pass
+        if self.to_plot:
+            info['main_asset'] = self.main_asset
+            self.env.render_graphs(self.ax, self.ax_volume, info)
+            self.fig.suptitle(f'Sim: {self.env.name}, Alg: {self.name}', fontsize=16)
+            plt.pause(0.001)

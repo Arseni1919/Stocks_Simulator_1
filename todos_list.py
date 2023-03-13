7b01d6605266b0cf320a8a91dd6fbe39f68e40be
perchik_air <1919ars@gmail.com>
2023-03-13 21:49:59 +0200

    more

--- todos_list.py
+++ todos_list.py
@@ -3,4 +3,4 @@
 # make an api for users - done
 # update holding worth to take into account all the assets - not now
 # update graphic of portfolio worth - done
-# TODO: make an object of a deal - what percentage of revenue is a deal
+# make an object of a deal - what percentage of revenue is a deal - later

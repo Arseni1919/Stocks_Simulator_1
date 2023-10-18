c79179a5930c1f763d0a3ca70f86e5bbba510a0e
perchik_air <1919ars@gmail.com>
2023-10-18 16:04:48 +0300

    more

--- globals.py
+++ globals.py
@@ -6,10 +6,13 @@ import datetime
 import json
 
 # import pymongo
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
 
 # import torch
 # import torch.nn as nn
